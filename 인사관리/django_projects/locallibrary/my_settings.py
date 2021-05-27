b40141857a1fd7ddfb74c59a7bbe97da0e07f733
jobum97 <51693368+jobum97@users.noreply.github.com>
2021-05-27 15:47:04 +0900

    15:30

---
+++ 인사관리/django_projects/locallibrary/my_settings.py
@@ -0,0 +1,12 @@
+
+DATABASES={
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'personnel_info',
+        'USER': 'root',
+        'PASSWORD':'Bs970923',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY='django-insecure-1&dp$8&=e&0)_o=+ewpmt3yyb1&g@d)%l&g5&22wekxx5tgq@-'
